db696dc59098f8eac260440895e53d51916c1c00
didierthomasm <didierthomas.m@gmail.com>
2023-10-27 11:37:53 -0600

    Update nodemon version to 3.0.1 and http-server version to 14.1.1.

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,17 +7,22 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+function generateCache(cacheName) {
+  return new CacheFirst({
+    cacheName: cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  });
+}
+
+const pageCache = generateCache('page-cache');
+const imageCache = generateCache('image-cache');
 
 warmStrategyCache({
   urls: ['/index.html', '/'],
@@ -26,5 +31,4 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => request.destination === 'image', imageCache);
