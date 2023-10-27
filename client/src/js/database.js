0c98eb87e105f905e9b1b903de5fc024c419cbf9
didierthomasm <didierthomas.m@gmail.com>
2023-10-27 12:09:03 -0600

    Update database.js to export the functions initdb, putDb, and getDb, and implement the logic for each function.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,52 @@
-import { openDB } from 'idb';
+import {openDB} from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+export const initdb = async () => {
+  const jateDb = await openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', {keyPath: 'id', autoIncrement: true});
       console.log('jate database created');
     },
   });
-
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+};
 
 initdb();
+
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+
+    const trans = jateDb.transaction('jate', 'readwrite');
+
+    const store = trans.objectStore('jate');
+
+    return store.add({content: content});
+
+  } catch (err) {
+    console.error('putDb not implemented', err)
+  }
+};
+
+
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+
+    const trans = jateDb.transaction('jate', 'readonly');
+
+    const store = trans.objectStore('jate');
+
+    const request = store.getAll();
+
+    const result = await request;
+
+    console.log('result', result);
+
+    return result;
+  } catch (err) {
+    console.error('getDb not implemented', err)
+  }
+};
